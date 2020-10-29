b13ef2991749a5c5fe812d87463861c621330e15
Rajesh Jha <jharajesh035@gmail.com>
2020-10-29 19:28:20 +0530

    Recursion

--- Recursion/CPP/Recursion_Part_10.cpp
+++ Recursion/CPP/Recursion_Part_10.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
@@ -29,9 +30,81 @@ void Lexographical_Order(int st, int end)
     }
 }
 
+int Letter_Tile_Possibilities(string str)
+{
+    if(str.length() == 0)
+    {
+        return 0;
+    }
+
+    int count = 0;
+    vector<bool> vis(26, false);
+    for(int i=0; i<str.length(); i++)
+    {
+        int chIdx = str[i] - 'A';
+        if(vis[chIdx] == false)
+        {
+            vis[chIdx] = true;
+            string nstr = str.substr(0, i) + str.substr(i+1);
+            count += Letter_Tile_Possibilities(nstr) + 1;
+        }
+    }
+    return count;
+}
+int Letter_Tile_Possibilities_01(string str)
+{
+    if(str.length() == 0)
+    {
+        return 0;
+    }
+
+    int count = 0;
+    int vis=0;
+    for(int i=0; i<str.length(); i++)
+    {
+        int mask = 1 << (str[i] - 'A'); 
+        if((vis & mask) == 0)
+        {
+            vis ^= mask;
+            string nstr = str.substr(0, i) + str.substr(i+1);
+            count += Letter_Tile_Possibilities_01(nstr) + 1;
+        }
+    }
+    return count;
+}
+int Letter_Tile_Possibilities_02(string str, string ans)
+{
+    cout << ans << endl;
+    if(str.length() == 0)
+    {
+        return 0;
+    }
+
+    int count = 0;
+    int vis=0;
+    for(int i=0; i<str.length(); i++)
+    {
+        int mask = 1 << (str[i] - 'A'); 
+        if((vis & mask) == 0)
+        {
+            vis ^= mask;
+            string nstr = str.substr(0, i) + str.substr(i+1);
+            count += Letter_Tile_Possibilities_02(nstr, ans+str[i]+" ") + 1;
+        }
+    }
+    return count;
+}
+void Leetcode_1079()
+{
+    //cout << Letter_Tile_Possibilities("AAB") << endl;
+    //cout << Letter_Tile_Possibilities_01("AAABBC") << endl;
+    cout << Letter_Tile_Possibilities_02("AAB", "") << endl;
+}
 void Questions_Set_01()
 {
-    Lexographical_Order(1, 20);
+    //Lexographical_Order(1, 20);
+
+    Leetcode_1079();
 }
 
 void solve()
--- Recursion/JAVA/Recursion_Part_10.java
+++ Recursion/JAVA/Recursion_Part_10.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Recursion_Part_10
 {
     public static void Lexicographical_Order(int st, int end)
@@ -26,9 +29,43 @@ public class Recursion_Part_10
             Lexicographical_Order(st+1, end);
         }
     }
+
+    public static List<String> res;
+    public static List<String> generateParenthesis(int n)
+    {
+        res = new ArrayList<>();
+
+        generateParenthesis("", 0, 0, n);
+
+        return res;
+    }
+    public static void generateParenthesis(String ans, int OB, int CB, int n)
+    {
+        if(OB+CB == 2*n)
+        {
+            res.add(ans);
+            return;
+        }
+        if(OB < n)
+        {
+            generateParenthesis(ans+"(", OB+1, CB, n);
+        }
+
+        if(CB < OB)
+        {
+            generateParenthesis(ans+")", OB, CB+1, n);
+        }
+    }
+    public static void Leetcode_22()
+    {
+        List<String> res = generateParenthesis(3);
+        System.out.println(res);
+    }
     public static void Questions_Set_01()
     {
-        Lexicographical_Order(1, 20);
+        //Lexicographical_Order(1, 20);
+
+        Leetcode_22();
     }
     public static void solve()
     {
