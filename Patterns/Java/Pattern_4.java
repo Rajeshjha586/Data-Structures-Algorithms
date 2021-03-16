9118993692943484afe9d500a6703e462b1cd5c3
Rajesh Jha <jharajesh035@gmail.com>
2021-03-16 22:25:31 +0530

    Pattern

--- Patterns/Java/Pattern_4.java
+++ Patterns/Java/Pattern_4.java
@@ -1,18 +1,17 @@
-public class Pattern_2{
-    public static void Pattern_2(int n)
+public class Pattern_4{
+    public static void Pattern_4(int n)
     {
         int sp = 0;
         int st = n;
-        for(int i=5; i>=1; i--)
+        for(int i=1; i<=5; i++)
         {
-            for(int j=1; j<=st; j++)
-            {
-                System.out.print("*\t");
-            }
-
             for(int j=1; j<=sp; j++)
             {
                 System.out.print("\t");
+            }
+            for(int j=1; j<=st; j++)
+            {
+                System.out.print("*\t");
             }
 
             sp++;
@@ -23,15 +22,15 @@ public class Pattern_2{
     public static void solve(){
         /**
         
-        * * * * * 
-        * * * * 
-        * * * 
-        * * 
-        * 
+        *	*	*	*	*	
+            *	*	*	*	
+                *	*	*	
+                    *	*	
+                        *	
         
         */
 
-        Pattern_2(5);
+        Pattern_4(5);
 
     }
     public static void main(String[] args)
